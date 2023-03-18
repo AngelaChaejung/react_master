fbc635c0958c26b09522b699782d39e0f71a0607
Angelachaejung <a01083298599@gmail.com>
2023-03-18 14:15:31 +0900

    Study/18Mar

---
+++ applecoding/src/Cart.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Table from "react-bootstrap/Table";
+
+const Cart = () => {
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thed></thed>
+      </Table>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ applecoding/src/Detail.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { Context1 } from "./Home";
+
+const Detail = () => {
+  const schedule = useContext(Context1);
+  return <div>디테일페이지지롱</div>;
+};
+
+export default Detail;
